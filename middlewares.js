393f6ff421758265eba9bea6239d75c7f6e11a5a
Meandering-Ninja <161959791+Meandering-Ninja@users.noreply.github.com>
2024-07-14 21:19:04 +0530

    Update middlewares.js

--- middlewares.js
+++ middlewares.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const server = express();
 server.use(express.json());
-server.use(express.static('public'));
+server.use(express.static('public')); //public is a folder here which contains an HTML page
 //for authentication(6-14)
 const auth =(req,res,next)=>{
     
@@ -17,7 +17,7 @@ const auth =(req,res,next)=>{
 
 //API or Endpoints or Route
 server.get('/',(req,res)=>{
-    res.send("Hello Pandya APni textile industry me partner banale")
+    res.send("Text as you want")
     // res.json({type:'GET'})
 })
 server.post('/',auth,(req,res)=>{
@@ -29,9 +29,7 @@ server.delete('/',(req,res)=>{
 server.patch('/',(req,res)=>{
     res.json({type:'PATCH'})
 })
-// server.get('/',(req,res)=>{
-//     res.send('Pandya Apni Textile industry me partner bana de')
-  
+
 // })
 server.listen(8080,()=>{
     console.log("Server Started");
